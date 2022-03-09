740dd2ec0d0347be5b119b187f671dbd941909b1
SouabeRaphael <R.souabe03@gmail.com>
2022-03-10 00:59:18 +0100

    deuxième page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Scroll header
+
 let contentHeader = document.querySelector('.content-header');
 
 window.addEventListener('scroll', addClassScroll);
@@ -10,23 +12,34 @@ function addClassScroll(){
     contentHeader.classList.remove('scroll');
   }
 }
+addClassScroll();
+
+// Menu burger
+
+let burger = document.querySelector(".menu-burger");
+let fullScreenMenu = document.querySelector(".menu-anim");
+
+burger.addEventListener("click", animBurger);
+
+function animBurger() {
+  burger.classList.toggle("active");
+  fullScreenMenu.classList.toggle("is-active");
+}
 
 // audio
 
+let song = document.querySelector(".my-song");
 
-let song = document.querySelector('.my-song');
+let iconSound = document.querySelector(".circle-icon-sound");
 
-let iconSound = document.querySelector('.circle-icon-sound');
+iconSound.addEventListener("click", getSound);
 
-iconSound.addEventListener('click', getSound);
-
-function getSound(){
-    if(song.paused){
-        song.play();
-        iconSound.innerHTML = `<i class="fa-solid fa-volume-high"></i>`;
-    }
-    else{
-        song.pause();
-        iconSound.innerHTML = `<i class="fa-solid fa-volume-xmark"></i>`;
-    }
+function getSound() {
+  if (song.paused) {
+    song.play();
+    iconSound.innerHTML = `<i class="fa-solid fa-volume-high"></i>`;
+  } else {
+    song.pause();
+    iconSound.innerHTML = `<i class="fa-solid fa-volume-xmark"></i>`;
+  }
 }
